98f105e39a8840f121a87cd6cc646114694a436e
Suhaib Abdulghani <snabieel@gmail.com>
2018-06-08 06:16:23 -0400

    Initial Commit

---
+++ detroitLabsChallenge/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  detroitLabsChallenge
+//
+//  Created by may on 2018-06-08.
+//  Copyright © 2018 sohaeb. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
